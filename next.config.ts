5ce247c5ef119cd31eda8acb9aefdf550507de09
Pierre Dok <pierredok@outlook.fr>
2025-10-09 12:42:52 +0200

    deploy progblem fix with turbo pack

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // other experimental options can go here
+  },
 };
 
 export default nextConfig;
