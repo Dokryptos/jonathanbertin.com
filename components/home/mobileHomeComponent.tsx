116adca685c56017dabab2ef63d404eb663165d3
Dok <pierredok@outlook.fr>
2025-10-11 19:27:34 +0200

    adding nimation mobile homepage

--- components/home/mobileHomeComponent.tsx
+++ components/home/mobileHomeComponent.tsx
@@ -4,6 +4,7 @@ import Grid from "../ui/grid/index";
 import HomeImgType from "@/types/homepage";
 import { UIImageSanity } from "../ui/image/sanity";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface HomeDataProps {
   homeData: HomeImgType[];
@@ -17,18 +18,28 @@ export const MobileHomeComponent = ({ homeData }: HomeDataProps) => {
       setIndexImg((prevIndex) =>
         prevIndex === homeData.length - 1 ? 0 : prevIndex + 1
       );
-    }, 1000);
+    }, 2000);
     return () => clearInterval(interval);
   }, [homeData.length]);
 
   return (
-    <Grid className="laptop:hidden h-dvh flex items-center justify-center pr-5 pl-5">
+    <Grid className="laptop:hidden h-dvh pr-5 pl-5">
+      <AnimatePresence mode="wait">
+        <motion.div           
+        key={homeData[indexImg]._id}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="w-auto h-auto flex items-center justify-center col-span-full tablet:col-span-6 tablet:col-start-3">
       <UIImageSanity
         key={homeData[indexImg]._id}
         asset={homeData[indexImg].thumbnail.asset._ref}
         alt={homeData[indexImg].title}
-        className="object-contain col-span-full tablet:col-span-6 tablet:col-start-3 "
+        className="object-contain "
       />
+      </motion.div>
+      </AnimatePresence>
     </Grid>
   );
 };
