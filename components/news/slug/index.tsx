efdf67d0bc81b873a42686433aa230d2576f7861
Pierre Dok <pierredok@outlook.fr>
2025-10-13 15:07:18 +0200

    news page tablet laptop desktop

--- components/news/slug/index.tsx
+++ components/news/slug/index.tsx
@@ -10,7 +10,7 @@ export default function NewsComponent({ newsData }: PersonnalProps) {
   const selectedNewsData = newsData[0];
 
   return (
-    <div className="grid grid-cols-1 tablet:grid-cols-2 pt-[80px] desktop  :pt-[96px] pl-5 pr-5">
+    <div className="grid grid-cols-1 tablet:grid-cols-2 pt-[80px] desktop:pt-[96px] pl-5 pr-5">
       <div className="font-bagossTrial text-[20px]/[18px] flex items-center pb-4 tablet:hidden">
         {selectedNewsData.title}
         <span className="text-[9px] tarblet:text-[12px] pl-2">{`(0${selectedNewsData.gallery.length})`}</span>
