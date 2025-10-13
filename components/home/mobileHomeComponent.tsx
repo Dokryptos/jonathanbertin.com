0f68f403439386eb8293eb6a05a3f3e37e309bd9
Pierre Dok <pierredok@outlook.fr>
2025-10-13 15:16:08 +0200

    deploy animation home mobile

--- components/home/mobileHomeComponent.tsx
+++ components/home/mobileHomeComponent.tsx
@@ -18,27 +18,28 @@ export const MobileHomeComponent = ({ homeData }: HomeDataProps) => {
       setIndexImg((prevIndex) =>
         prevIndex === homeData.length - 1 ? 0 : prevIndex + 1
       );
-    }, 2000);
+    }, 1500);
     return () => clearInterval(interval);
   }, [homeData.length]);
 
   return (
     <Grid className="laptop:hidden h-dvh pr-5 pl-5">
       <AnimatePresence mode="wait">
-        <motion.div           
-        key={homeData[indexImg]._id}
+        <motion.div
+          key={homeData[indexImg]._id}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.3, ease: "easeInOut" }}
-          className="w-auto h-auto flex items-center justify-center col-span-full tablet:col-span-6 tablet:col-start-3">
-      <UIImageSanity
-        key={homeData[indexImg]._id}
-        asset={homeData[indexImg].thumbnail.asset._ref}
-        alt={homeData[indexImg].title}
-        className="object-contain "
-      />
-      </motion.div>
+          transition={{ duration: 0.15, ease: "easeInOut" }}
+          className="w-auto h-auto flex items-center justify-center col-span-full tablet:col-span-6 tablet:col-start-3"
+        >
+          <UIImageSanity
+            key={homeData[indexImg]._id}
+            asset={homeData[indexImg].thumbnail.asset._ref}
+            alt={homeData[indexImg].title}
+            className="object-contain "
+          />
+        </motion.div>
       </AnimatePresence>
     </Grid>
   );
