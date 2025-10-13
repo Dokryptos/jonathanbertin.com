70931366c3d339ae7ded56c8e19a8f9c35e1b3f1
Pierre Dok <pierredok@outlook.fr>
2025-10-13 15:06:10 +0200

    news page tablet laptop desktop

--- components/news/slug/index.tsx
+++ components/news/slug/index.tsx
@@ -10,8 +10,8 @@ export default function NewsComponent({ newsData }: PersonnalProps) {
   const selectedNewsData = newsData[0];
 
   return (
-    <div className="grid grid-cols-1 tablet:grid-cols-2 pt-[80px] desktop:pt-[96px] pl-5 pr-5">
-      <div className="font-bagossTrial text-[16px]/[18px] flex items-center pb-4 tablet:hidden">
+    <div className="grid grid-cols-1 tablet:grid-cols-2 pt-[80px] desktop  :pt-[96px] pl-5 pr-5">
+      <div className="font-bagossTrial text-[20px]/[18px] flex items-center pb-4 tablet:hidden">
         {selectedNewsData.title}
         <span className="text-[9px] tarblet:text-[12px] pl-2">{`(0${selectedNewsData.gallery.length})`}</span>
       </div>
@@ -31,9 +31,9 @@ export default function NewsComponent({ newsData }: PersonnalProps) {
         ))}
       </div>
       <div className="whitespace-pre-wrap text-[14px]/[20px] pt-3 tablet:pt-0 pl-[6px] laptop:pl-6 desktop:pl-[65px] laptop:pr-[51px] desktop:pr-[133px] overflow-y-auto scrollbar-hide tablet:max-h-[calc(100vh-80px)] desktop:max-h-[calc(100vh-96px)]">
-        <div className="font-bagossTrial text-[16px]/[18px] items-center pb-4 hidden tablet:flex ">
+        <div className="font-bagossTrial text-[20px]/[18px] desktop:text-[25px] items-center pb-4 hidden tablet:flex ">
           {selectedNewsData.title}
-          <span className="text-[9px] tarblet:text-[12px] pl-2">{`(0${selectedNewsData.gallery.length})`}</span>
+          <span className="text-[9px] tablet:text-[12px] pl-2">{`(0${selectedNewsData.gallery.length})`}</span>
         </div>
         {selectedNewsData.description}
       </div>
