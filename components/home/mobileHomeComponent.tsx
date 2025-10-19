f1b03a753221ceff150538bd9125f32f7a217962
Pierre Dok <pierredok@outlook.fr>
2025-10-19 22:55:14 +0200

    animation homepage mobile

--- components/home/mobileHomeComponent.tsx
+++ components/home/mobileHomeComponent.tsx
@@ -4,7 +4,6 @@ import Grid from "../ui/grid/index";
 import HomeImgType from "@/types/homepage";
 import { UIImageSanity } from "../ui/image/sanity";
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 
 interface HomeDataProps {
   homeData: HomeImgType[];
@@ -24,23 +23,17 @@ export const MobileHomeComponent = ({ homeData }: HomeDataProps) => {
 
   return (
     <Grid className="laptop:hidden h-dvh pr-5 pl-5">
-      <AnimatePresence mode="wait">
-        <motion.div
+      <div
+        key={homeData[indexImg]._id}
+        className="w-auto h-auto flex items-center justify-center col-span-full tablet:col-span-6 tablet:col-start-3"
+      >
+        <UIImageSanity
           key={homeData[indexImg]._id}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0, ease: "easeInOut" }}
-          className="w-auto h-auto flex items-center justify-center col-span-full tablet:col-span-6 tablet:col-start-3"
-        >
-          <UIImageSanity
-            key={homeData[indexImg]._id}
-            asset={homeData[indexImg].thumbnail.asset._ref}
-            alt={homeData[indexImg].title}
-            className="object-contain "
-          />
-        </motion.div>
-      </AnimatePresence>
+          asset={homeData[indexImg].thumbnail.asset._ref}
+          alt={homeData[indexImg].title}
+          className="object-contain "
+        />
+      </div>
     </Grid>
   );
 };
